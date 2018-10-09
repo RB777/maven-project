2528bd42ddcaafc359860befedac7c09447fa783
Raju Bhana <rajubhana@Rajus-MacBook-Pro.local>
2018-10-09 13:15:36 +0200

    upated spaces and comments

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,12 +11,11 @@ public class Greeter {
   public Greeter() {
 
   }
-  
-  /*
-   * @param someone
-   * @return greeting 
-  */
 
+  /**
+   * @param someone
+   * @return greeting string
+  */
   public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
